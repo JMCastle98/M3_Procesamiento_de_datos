14d28965183678f722cd3beb84cff23d6538483f
LKXoro <71840465+LKXoro@users.noreply.github.com>
2021-01-23 21:33:44 -0600

    Add files via upload

---
+++ Postwork5/Postwork5.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(fbRanks)
+
+setwd("C:/Users/Diego/Documents/RStudio/P5/")
+
+smalldata <- read.csv("resultado.csv")
+smalldata <- select(smalldata,Date:FTAG)
+smalldata <- rename(smalldata, date = Date, home.team = HomeTeam, away.team = AwayTeam, home.score = FTHG, away.score = FTAG)
+
+write.csv(smalldata,row.names = F,"soccer.csv")
+
+library(fbRanks)
+
+listasoccer <- create.fbRanks.dataframes("soccer.csv")
+anotaciones <- listasoccer$scores
+equipos <- listasoccer$teams
+
+fecha <- unique(anotaciones$date)
+n <- length(fecha)
+
+ranking <- rank.teams(scores = anotaciones,teams = equipos,max.date = "2020-07-16",min.date = "2017-08-18")
+
+
+predict(ranking, date=fecha[n])
