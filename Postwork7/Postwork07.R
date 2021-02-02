b95f9be306a5ea535849598534129b19843189b1
Juan Miguel Ovando Castillo <73368459+JMCastle98@users.noreply.github.com>
2021-02-02 17:43:40 -0600

    Update Postwork07.R

--- Postwork7/Postwork07.R
+++ Postwork7/Postwork07.R
@@ -40,7 +40,39 @@ match$count(
   query = '{"$or" : [{"AwayTeam": "Real Madrid"},{"HomeTeam": "Real Madrid"}]}'
 )
 
-#El real jugó un total de 114 partidos pero ninguno en la fecha solicita
+#El real jugó un total de 114 partidos pero ninguno en la fecha solicitada
+
+#Proponemos incluir el dataset de la temporada 2015-2016
+
+setwd("C:/.../DataSets")
+
+SP2015_2016 <- read.csv("SP2015_2016.csv")
+
+SP2015_2016 <- mutate(SP2015_2016, Date = as.Date(Date, "%d/%m/%y"))
+
+#Filtramos los campos correctos del data frame
+SP2015_2016 <-SP2015_2016 %>% 
+  select(Date,HomeTeam,AwayTeam,FTHG,FTAG,FTR)
+
+#El método insert nos permite añadir documentos a la colección
+  
+match$insert(SP2015_2016)  
+
+#Nuevo conteo de documentos totales
+match$count()
+
+#Encontramos los partidos de esa fecha
+match$find(
+  query = '{"Date": "2015-12-20"}'
+)
+
+#Buscamos si en esa fecha el Real Madrid jugó como visitante o en casa
+match$find(
+  query = '{"$and" : [{"Date": "2015-12-20"},
+  {"$or" : [{"AwayTeam": "Real Madrid"},{"HomeTeam": "Real Madrid"}]}]}'
+)
+
+#El real Madrid ganó por goleada ese día
 
 match$disconnect( gc = TRUE)
 rm(match)
